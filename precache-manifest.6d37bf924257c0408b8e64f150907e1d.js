3e2990fcb2803008af5b95fca5ff884e251d4502
ParkerZA <parkerwaseem@gmail.com>
2019-02-14 17:04:28 +0200

    Updates

---
+++ precache-manifest.6d37bf924257c0408b8e64f150907e1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5650b9d376d90228db4",
+    "url": "/Styler/static/js/runtime~main.c5650b9d.js"
+  },
+  {
+    "revision": "64963347269c480f5935",
+    "url": "/Styler/static/js/main.64963347.chunk.js"
+  },
+  {
+    "revision": "e5ff118cb3fcdc4cea89",
+    "url": "/Styler/static/js/1.e5ff118c.chunk.js"
+  },
+  {
+    "revision": "64963347269c480f5935",
+    "url": "/Styler/static/css/main.7dc19b49.chunk.css"
+  },
+  {
+    "revision": "e5ff118cb3fcdc4cea89",
+    "url": "/Styler/static/css/1.72bd544e.chunk.css"
+  },
+  {
+    "revision": "13330827fea485b59a7a457b8d883780",
+    "url": "/Styler/index.html"
+  }
+];
